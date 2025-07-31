8eaf8cff51d7498eed46f94dc651f7edcb24abc1
ziad <zezo97736@gmail.com>
2025-07-31 18:57:52 +0300

    add Repository interface

---
+++ app/src/main/java/com/ziad/data/repository/PostsRepository.kt
@@ -0,0 +1,24 @@
+package com.ziad.data.repository
+
+import com.ziad.data.model.Post
+import com.ziad.utils.Result
+import java.io.File
+
+interface PostsRepository {
+    suspend fun getAllPosts(): List<Post>
+    suspend fun getPost(id:Int): Post
+    suspend fun storePost(
+        photo: File,
+        title: String,
+        content: String
+    ): Result
+    suspend fun updatePost(
+        id: Int,
+        photo: File,
+        title: String,
+        content: String
+    ): Result
+    suspend fun deletePost(
+        id: Int
+    ): String?
+}
