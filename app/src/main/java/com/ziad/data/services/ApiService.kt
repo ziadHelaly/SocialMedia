f85c5beba06d1b51fd6b36dcad4d495f060b866d
ziad <zezo97736@gmail.com>
2025-07-31 18:22:20 +0300

    add application subclass

---
+++ app/src/main/java/com/ziad/data/services/ApiService.kt
@@ -0,0 +1,38 @@
+package com.ziad.data.services
+
+import com.ziad.data.model.Post
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET
+    suspend fun getPosts(): List<Post>
+
+    @GET("show/{id}")
+    suspend fun getPostById(@Path("id") id: Int): Post
+
+    @Multipart
+    @POST("store")
+    suspend fun uploadPost(
+        @Part photo: MultipartBody.Part,
+        @Part("title") title: RequestBody,
+        @Part("content") content: RequestBody
+    ): Response<Post>
+
+    @POST("update/{id}")
+    suspend fun updatePost(
+        @Path("id") id: Int,
+        @Part photo: MultipartBody.Part,
+        @Part("title") title: RequestBody,
+        @Part("content") content: RequestBody
+    ): Response<Post>
+
+    @POST("delete/{id}")
+    suspend fun deletePost(@Path("id") id: Int)
+}
---
+++ app/src/main/java/com/ziad/di/SocialMediaApplication.kt
@@ -0,0 +1,7 @@
+package com.ziad.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SocialMediaApplication: Application()
