1adb0e7f2e32bb403d078aaf94d562baa96fe607
ziad <zezo97736@gmail.com>
2025-08-01 23:01:58 +0300

    add helper function to get file from uri

--- app/src/main/java/com/ziad/utils/PartHelperes.kt
+++ app/src/main/java/com/ziad/utils/PartHelperes.kt
@@ -1,11 +1,14 @@
 package com.ziad.utils
 
+import android.content.Context
+import android.net.Uri
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
+import java.io.IOException
 
 
 fun createPartFromString(value: String): RequestBody =
@@ -20,4 +23,13 @@ fun prepareFilePart(partName: String, file: File): MultipartBody.Part {
 
     val requestFile = file.asRequestBody(mediaType)
     return MultipartBody.Part.createFormData(partName, file.name, requestFile)
+}
+fun uriToFile(context: Context, uri: Uri, outputName: String): File {
+    val inputStream = context.contentResolver.openInputStream(uri)
+        ?: throw IOException("Unable to open URI")
+    val tempFile = File(context.cacheDir, outputName)
+    tempFile.outputStream().use { output ->
+        inputStream.copyTo(output)
+    }
+    return tempFile
 }
